18803068332d0e4cb647c3dfc663b867ae391a6a
zecheeseking <christopher.dale.@student.howest.be>
2017-01-05 00:43:47 +0100

    linkoptions for genie script

--- genie.lua
+++ genie.lua
@@ -33,7 +33,7 @@ PROJ_DIR = path.getabsolute("..")
             defines {"PLATFORM_WIN", "WIN32"}
 
         configuration "gmake"
-            buildoptions {"-std=C++11"}
+            linkoptions {"'-std=C++11'"}
             targetdir "bin/debug"
             objdir "obj/debug"
             flags {"Unicode"}
