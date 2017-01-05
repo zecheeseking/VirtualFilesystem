9f06cf1e53272f5e936732c879d88a7210a33a27
zecheeseking <christopher.dale.@student.howest.be>
2017-01-05 16:32:53 +0100

    Fixing accidental commit of Linux build files.

--- obj/debug/x32/Debug/File.d
+++
@@ -1,5 +0,0 @@
-obj/debug/x32/Debug/File.o: File.cpp File.h FileMode.h
-
-File.h:
-
-FileMode.h:
--- obj/debug/x32/Debug/main.d
+++
@@ -1,7 +0,0 @@
-obj/debug/x32/Debug/main.o: main.cpp FileSystem.h File.h FileMode.h
-
-FileSystem.h:
-
-File.h:
-
-FileMode.h:
