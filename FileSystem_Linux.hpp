a1505f03087aa27ed8cdb30e076041e0b232ffec
zecheeseking <christopher.dale.@student.howest.be>
2017-01-05 15:57:41 +0100

    First pass at MountDirectory() for Linux platform, implemented LogAllFiles() as well.

--- FileSystem_Linux.hpp
+++ FileSystem_Linux.hpp
@@ -1,6 +1,7 @@
 #include "FileSystem.h"
 
 #include <iostream>
+#include <dirent.h>
 
 FileSystem::FileSystem() {}
 
@@ -9,23 +10,50 @@ FileSystem::~FileSystem() {}
 void FileSystem::Test() {}
 
 void FileSystem::MountDirectory(const std::string & directory) {
-    std::cout << "Linux MountDirectory implementation needed!\n";
+    DIR * dir = opendir(directory.c_str());
+
+    if(dir == nullptr)
+        std::cout << "Cannot process directory" << std::endl;
+
+    auto entity = readdir(dir);
+
+    while(entity)
+    {
+        //Check if directory
+        if(entity->d_type == DT_DIR)
+        {
+            if(entity->d_name[0] == '.')
+            {
+                continue;
+            }
+            else
+                MountDirectory(directory + entity->d_name);
+        }
+        //Else check if file.
+        else if(entity->d_type == DT_REG)
+        {
+            m_Files.push_back(directory + entity->d_name);
+        }
+    }
+
+    closedir(dir);
 }
 
 std::string FileSystem::GetPhysicalFilePath(const std::string & filename) const {
-   std::cout << "Linux MountDirectory implementation needed!\n";
-   return nullptr;
+   std::cout << "Linux GetPhysicalFilePath implementation needed!\n";
+   return "";
 }
 
 void FileSystem::GetFilesInDirectory(std::vector<File>& fileTable, const std::string & directory)const {
-   std::cout << "Linux MountDirectory implementation needed!\n";
+   std::cout << "Linux GetFilesInDirectory implementation needed!\n";
 }
 
 void FileSystem::GetFilesWithExtension(std::vector<File>& fileTable, const std::string & extension)const {
-   std::cout << "Linux MountDirectory implementation needed!\n";
+   std::cout << "Linux GetFilesWithExtension implementation needed!\n";
 }
 
 void FileSystem::LogAllFiles() const
 {
-    std::cout << "Linux MountDirectory implementation needed!\n";
+    for(auto f : m_Files)
+        std::cout << f.mPhysicalPath << std::endl;
 };
